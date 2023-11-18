ca2dde169e7fb1b1e132862ba5d378d6fbab65a1
Anton Sankov <asankov96@gmail.com>
2023-11-18 15:01:47 +0200

    Initialize cobra cli app scaffold

---
+++ cmd/root.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	cfgFile  string
+	senderID string
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "secure-messenger",
+	Short: "secure-messenger allows you to send and receive encrypted messages",
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().StringVar(&cfgFile, "config-file", "~/.config/secure-messenger/config.yaml", "file from which to read the config values")
+	rootCmd.Flags().StringVar(&senderID, "sender-id", "", "your Sender ID")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/asankov/secure-messenger/cmd"
+
+func main() {
+	cmd.Execute()
+}
